794a656895e755a38c15d0f73b6098a1c39a7d4f
minhle10 <lems@bc.edu>
2023-06-12 08:49:40 -0400

    comments + tags + tailwind css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
+  http_basic_authenticate_with :name =>"minh", :password => "password", :except => [:index, :show]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_154637) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_135402) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -19,4 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_154637) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tags_on_post_id"
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "tags", "posts"
 end
